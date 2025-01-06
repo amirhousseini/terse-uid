5c92d441fa5aa5e698370c6094415fec4a7ca3ef
amirhousseini <123155557+amirhousseini@users.noreply.github.com>
2025-01-06 10:46:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * Module providing a single function that return unique identifiers (UID)
  * in the form of strings of 16 alphanumerical characters, all lowercase.
  * To achieve uniqueness UIDs are built from the following number components:
- *  - an incrementing counter initialized to some random number,
+ *  - an round-robin counter initialized to some random number,
  *  - the current time in milliseconds since 2025-01-01 00:00:00.000,
- *  - a context value derived from the process identifier, the thread identifiers, and a random number.
+ *  - a context value derived from the process identifier, the thread identifier, and a random number.
  * The UIDs are not guaranteed unique in a universal sense.
  * But duplicates are practically impossible.
  */
@@ -34,7 +34,7 @@ var counter = randomInt(maxCounter);
 
 /**
  * Return a random integer between 0 (inclusive) and the given maximum value (exclusive).
- * @param {number} maxInt Maximum number of digits
+ * @param {number} maxInt Maximum value
  * @returns Number
  */
 function randomInt(maxInt) {
@@ -43,7 +43,7 @@ function randomInt(maxInt) {
 
 /*
  * Encode a number into a string of alphanumerical characters of a given length.
- * The string is truncated if the value exceeds the maximum number the length can accomodate.
+ * The string is truncated to the specified length.
  * @param {number} value Number to encode
  * @param {number} length Length of the resulting character string
  * @returns String
@@ -80,9 +80,9 @@ function getTimeComponent() {
  * @returns String
  */
 function getContextComponent() {
-    let rid = randomInt(100000);            // Random value in the range 0...999
-    let tid = threadId + 100000;            // Thread identifier in the range 1000..99'999
-    let pid = process.pid + 2 * 100000;     // Process identifier in the range 100'000 and above
+    let rid = randomInt(100000);       // Random value in the range 0...99'999
+    let tid = threadId + 100000;       // Thread identifier in the range 100'000..199'999
+    let pid = process.pid + 200000;    // Process identifier in the range 200'000 and above
     return encode(pid + tid + rid, contextLen);
 }
 
